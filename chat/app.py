f8e141561e388520a537eed7c86f8bcab5db1910
Valter <valtercostaf@gmail.com>
2024-07-25 09:51:48 -0300

    Primeiros passos.

---
+++ chat/app.py
@@ -0,0 +1,8 @@
+""" Chat - Módulo tal que faz isso.
+
+O módulo main faz isso e aquilo. Ele, de vez em quando, quase nunca, mais com frequência, parece que não é.
+
+Área do quadrado = lado x lado
+
+"""
+
