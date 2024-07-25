6fbbc979ae28b3ad83219e6f87e8df10f8cc92bd
Valter <valtercostaf@gmail.com>
2024-07-25 11:07:18 -0300

    Métodos get e set com decoradores.

--- main.py
+++ main.py
@@ -2,35 +2,52 @@
 
 class Aluno:
     def __init__(self):
-        self._nome = ''
-        self._matricula = ''
-        self._telefone = ''
+        self.__nome = ''
+        self.__matricula = ''
+        self.__telefone = ''
 
     def set_nome(self, novo_nome):
         if novo_nome != '':
-            self._nome = novo_nome
+            self.__nome = novo_nome
     
     def get_nome(self):
-        return self._nome
+        return self.__nome
     
     @property
     def matricula(self):
-        return self._matricula
+        return self.__matricula
     
     @matricula.setter
     def matricula(self,matricula):
         if matricula.isalnum() and not matricula.isalpha() and not matricula.isnumeric():
-            self._matricula=matricula
+            self.__matricula=matricula
         else:
             print("A matrícula deve ser alfanumérica, necessariamente...")
     
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self, telefone):
+        self.__telefone = telefone
+    
     def descreva_aluno(self):
-        return f'Nome: {self._nome}\n\
-            - Telefone: {self._telefone}\n\
-            - Matrícula: {self._matricula}'
+        return f'Nome: {self.__nome}\n\
+            - Telefone: {self.__telefone}\n\
+            - Matrícula: {self.__matricula}'
+
+class AlunoExtensionista(Aluno):
+    def __init__(self):
+        super().__init__()
+        self.__nome_curso = ''
+
     
 
 aluno1 = Aluno()
 aluno1.set_nome("Raimundo Valter")
 aluno1.matricula = '123a'
+aluno1.__telefone = '85 9999 11 22'
 print(aluno1.descreva_aluno())
+
+aluno2 = AlunoExtensionista()
